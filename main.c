7a046b519ccd66fd07dbacb8a67464a405975968
Anderson <anderfrasao52@gmail.com>
2023-04-18 17:21:03 -0300

    fazer mais testes

--- main.c
+++ main.c
@@ -35,7 +35,31 @@ int ** lerArquivo(int *n, int *m, int *k, FILE* arquivo){
     return matriz;
 }
 
-void main(int argc, char **argv){
+int dfs(
+    int i, int j, int val, int ** vizitados, 
+    int ** mat, int m, int n
+) {
+    vizitados[i][j] = 1;
+    int max_depth = 0;
+    if (i == 0 || i == n-1 || j == 0 || j == m-1) {
+        max_depth = 1;
+    }
+    int dirs[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    for (int d = 0; d < 4; d++) {
+        int ni = i + dirs[d][0];
+        int nj = j + dirs[d][1];
+        if (ni >= 0 && ni < n && nj >= 0 && nj < m && !vizitados[ni][nj] && mat[ni][nj] == val) {
+            int depth = dfs(ni, nj, val, vizitados, mat, m, n);
+            if (depth > max_depth) {
+                max_depth = depth;
+            }
+        }
+    }
+    vizitados[i][j] = 0;
+    return max_depth + 1;
+}
+
+int main(int argc, char **argv){
     int n = 0;
     int m = 0;
     int k = 0;
@@ -49,9 +73,56 @@ void main(int argc, char **argv){
 
     arquivo = lerArquivo(&n, &m, &k, nome_arquivo);
 
-    
+    // Matriz de vizitados
+    int ** vizitados = (int**)calloc(n, sizeof(int*));
+    if(vizitados == NULL){
+        perror("Erro ao alocar vetor de int");
+        exit(1);
+    }
+    for(int i = 0; i < n; i++){
+        vizitados[i] = (int*)calloc(m, sizeof(int));
+        if(vizitados[i] == NULL){
+            perror("Erro ao alocar vetor de int");
+            exit(1);
+        }
+    }
+
+    // inicialização do array de profundidades máximas
+    int profundidade_max = -1;
+
+    int chosen_value = -1;
+    int chosen_i = -1;
+    int chosen_j = -1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            for (int v = 1; v <= k; v++) {
+                if (arquivo[i][j] != v) {
+                    int depth = dfs(i, j, v, vizitados, arquivo, m, n);
+                    if (depth > profundidade_max) {
+                        profundidade_max = depth;
+                        chosen_value = v;
+                        chosen_i = i;
+                        chosen_j = j;
+                    }
+                }
+            }
+        }
+    }
+    printf("Chosen value: %d\n", chosen_value);
+    printf("Chosen corner: (%d, %d)\n", chosen_i, chosen_j);
+
+    if (chosen_i == 0 && chosen_j == 0) 
+        printf("Canto escolhido: canto superior esquerdo\n");
+    else if (chosen_i == 0 && chosen_j == m-1)
+        printf("Canto escolhido: canto superior direito\n");
+    else if (chosen_i == n-1 && chosen_j == 0)
+        printf("Canto escolhido: canto inferior esquerdo\n");
+    else if (chosen_i == n-1 && chosen_j == m-1)
+        printf("Canto escolhido: canto inferior direito\n");
 
     for (int i = 0; i < n; i++)
         free(arquivo[i]);
     free(arquivo);
+
+    return 0;
 }
