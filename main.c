93a530690314750b1c65504060ca511352b20ae8
Anderson <anderfrasao52@gmail.com>
2023-04-19 15:22:36 -0300

    tem que arrumar o loop infinito!

--- main.c
+++ main.c
@@ -44,7 +44,7 @@ bool is_valid(int i, int j, int val, int ** mat, int m, int n) {
 }
 
 // Busca em profundidade, retorna a maior profundidade
-int dfs(int i, int j, int val, int ** mat, int m, int n, int cor) {
+void dfs(int i, int j, int val, int ** mat, int m, int n, int cor) {
     int max_depth = 0;
     int *stack_i = (int *)calloc(m*n, sizeof(int));
     int *stack_j = (int *)calloc(m*n, sizeof(int));
@@ -87,8 +87,6 @@ int dfs(int i, int j, int val, int ** mat, int m, int n, int cor) {
     free(stack_i);
     free(stack_j);
     free(stack_d);
-
-    return max_depth;
 }
 
 // Busca em profundidade, retorna o numero de casas preenchidas
@@ -139,66 +137,78 @@ int dfs2(int i, int j, int val, int ** mat, int m, int n) {
 }
 
 void search_corners(int ** mat, int n, int m, int k) {
-
-    // Matriz aux
-    int ** matriz1 = (int**)calloc(n, sizeof(int*));
-    if(matriz1 == NULL){
-        perror("Erro ao alocar vetor de int");
-        exit(1);
-    }
-    for(int i = 0; i < n; i++){
-        matriz1[i] = (int*)calloc(m, sizeof(int));
-        if(matriz1[i] == NULL){
+    int preenchidos = 0;
+    while(preenchidos <= m*n){
+        // Matriz aux
+        int ** matriz1 = (int**)calloc(n, sizeof(int*));
+        if(matriz1 == NULL){
             perror("Erro ao alocar vetor de int");
             exit(1);
         }
-    }
+        for(int i = 0; i < n; i++){
+            matriz1[i] = (int*)calloc(m, sizeof(int));
+            if(matriz1[i] == NULL){
+                perror("Erro ao alocar vetor de int");
+                exit(1);
+            }
+        }
 
-    for(int i = 0; i < n; i++)
-        for(int l = 0; l < m; l++)
-            matriz1[i][l] = mat[i][l];
+        for(int i = 0; i < n; i++)
+            for(int l = 0; l < m; l++)
+                matriz1[i][l] = mat[i][l];
 
-    int max_depth1 = -1;
-    int max_depth2 = -1;
-    int chosen_i = -1, chosen_j = -1, chosen_val = -1;
-    int di[] = {0, 0, n-1, n-1};
-    int dj[] = {0, m-1, 0, m-1};
-    // Testando todas as cores
-    for (int val = 1; val <= k; val++) {
-        // Passando pelos 4 cantos
-        for (int d = 0; d < 4; d++){
-            // Verifica se a cor escolhida ja não esta no canto
-            if (mat[di[d]][dj[d]] != val) {
-                // Faz a busca 
-                int depth1 = dfs(di[d], dj[d], mat[di[d]][dj[d]], mat, m, n, val);
-                int depth2 = dfs2(di[d], dj[d], val, mat, m, n);
+        int max_depth1 = -1;
+        int max_depth2 = -1;
+        int chosen_i = -1, chosen_j = -1, chosen_val = -1;
+        int di[] = {0, 0, n-1, n-1};
+        int dj[] = {0, m-1, 0, m-1};
+        // Testando todas as cores
+        for (int val = 1; val <= k; val++) {
+            // Passando pelos 4 cantos
+            for (int d = 0; d < 4; d++){
+                // Verifica se a cor escolhida ja não esta no canto
+                if (mat[di[d]][dj[d]] != val) {
+                    // Faz a busca 
+                    dfs(di[d], dj[d], mat[di[d]][dj[d]], mat, m, n, val);
+                    int depth2 = dfs2(di[d], dj[d], val, mat, m, n);
+                    for(int i = 0; i < n; i++)
+                        for(int l = 0; l < m; l++)
+                            mat[i][l] = matriz1[i][l];
+                    // Se a busca encontrou um caminho mais fundo que o anterior, salva
+                    if (depth2 > max_depth2) {
+                        max_depth2 = depth2;
+                        chosen_i = di[d];
+                        chosen_j = dj[d];
+                        chosen_val = val;
+                    }
+                }
                 for(int i = 0; i < n; i++)
                     for(int l = 0; l < m; l++)
                         mat[i][l] = matriz1[i][l];
-                // Se a busca encontrou um caminho mais fundo que o anterior, salva
-                if (depth2 > max_depth2) {
-                    max_depth1 = depth1;
-                    max_depth2 = depth2;
-                    chosen_i = di[d];
-                    chosen_j = dj[d];
-                    chosen_val = val;
-                }
             }
-            for(int i = 0; i < n; i++)
-                for(int l = 0; l < m; l++)
-                    mat[i][l] = matriz1[i][l];
         }
-    }
 
-    if(chosen_i == 0 && chosen_j == 0)
-        printf("a ");
-    else if(chosen_i == 0 && chosen_j == m-1)
-        printf("b ");
-    else if(chosen_i == n-1 &&  chosen_j == 0)
-        printf("c ");
-    else if(chosen_i == n-1 &&  chosen_j == m-1)
-        printf("d ");
-    printf("%d\n", chosen_val);
+        // O ERRO PROVAVELMENTE ESTA AQUI!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+
+        dfs(chosen_i, chosen_j, mat[chosen_i][chosen_j], mat, m, n, chosen_val);
+        preenchidos = dfs2(chosen_i, chosen_j, chosen_val, mat, m, n);
+
+        for(int i = 0; i < n; i++)
+            for(int l = 0; l < m; l++)
+                if(mat[i][l] == -1)
+                    mat[i][l] = chosen_val;
+
+        printf("%d\n", preenchidos);
+        // if(chosen_i == 0 && chosen_j == 0)
+        //     printf("a ");
+        // else if(chosen_i == 0 && chosen_j == m-1)
+        //     printf("b ");
+        // else if(chosen_i == n-1 &&  chosen_j == 0)
+        //     printf("c ");
+        // else if(chosen_i == n-1 &&  chosen_j == m-1)
+        //     printf("d ");
+        // printf("%d ", chosen_val);
+    }
 }
 
 int main(int argc, char **argv){
