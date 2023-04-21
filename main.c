7fac4d362ac9842f9d7e072303eb7faa564e8362
Anderson <anderfrasao52@gmail.com>
2023-04-20 21:32:38 -0300

    funciona e vai jogar no vasco

--- main.c
+++ main.c
@@ -10,6 +10,12 @@ typedef struct {
     int teste;
 } posicao;
 
+typedef struct {
+    char canto;
+    int cor;
+} passo;
+
+
 posicao ** lerArquivo(int *n, int *m, int *k, FILE* arquivo){
     
     if(arquivo == NULL){
@@ -61,7 +67,15 @@ bool is_valid2(int i, int j, int val, posicao ** mat, int m, int n) {
 // Busca em profundidade, retorna a maior profundidade
 void dfs(int i, int j, int val, posicao ** mat, int m, int n, int cor) {
     int *stack_i = (int *)calloc(m*n, sizeof(int));
+    if(stack_i == NULL){
+        perror("Erro ao alocar vetor de int");
+        exit(1);
+    }
     int *stack_j = (int *)calloc(m*n, sizeof(int));
+    if(stack_j == NULL){
+        perror("Erro ao alocar vetor de int");
+        exit(1);
+    }
     int stack_top = 0;
 
     stack_i[stack_top] = i;
@@ -72,9 +86,7 @@ void dfs(int i, int j, int val, posicao ** mat, int m, int n, int cor) {
         int cj = stack_j[stack_top];
         stack_top--;
 
-        // O ERRO ESTA AQUI, PARECIDO COM dfs2, ele troca o valor da matriz 
-        // e falha no teste de igualdade
-        if (is_valid2(ci, cj, val, mat, m, n)) {
+        if (is_valid(ci, cj, val, mat, m, n)) {
             mat[ci][cj].teste = -1;
             mat[ci][cj].cor = cor;
 
@@ -83,12 +95,10 @@ void dfs(int i, int j, int val, posicao ** mat, int m, int n, int cor) {
             for (int d = 0; d < 4; d++) {
                 int ni = ci + di[d];
                 int nj = cj + dj[d];
-                if (is_valid2(ni, nj, val, mat, m, n)) {
+                if (is_valid(ni, nj, val, mat, m, n)) {
                     stack_top++;
                     stack_i[stack_top] = ni;
                     stack_j[stack_top] = nj;
-                    mat[ni][nj].teste = -1;
-                    mat[ni][nj].cor = cor;
                 }
             }
         }
@@ -101,7 +111,15 @@ void dfs(int i, int j, int val, posicao ** mat, int m, int n, int cor) {
 // Busca em profundidade, retorna o numero de casas preenchidas
 int dfs2(int i, int j, int val, posicao ** mat, int m, int n) {
     int *stack_i = (int *)calloc(m*n, sizeof(int));
+    if(stack_i == NULL){
+        perror("Erro ao alocar vetor de int");
+        exit(1);
+    }
     int *stack_j = (int *)calloc(m*n, sizeof(int));
+    if(stack_j == NULL){
+        perror("Erro ao alocar vetor de int");
+        exit(1);
+    }
     int stack_top = 0;
     int preenchidos = 0;
 
@@ -142,23 +160,28 @@ int dfs2(int i, int j, int val, posicao ** mat, int m, int n) {
     return preenchidos;
 }
 
-void search_corners(posicao ** mat, int n, int m, int k) {
-    int preenchidos = 0;
-    while(preenchidos <= m*n){
-        // Matriz aux
-        posicao ** matriz1 = (posicao**)calloc(n, sizeof(posicao*));
-        if(matriz1 == NULL){
+passo * search_corners(posicao ** mat, int n, int m, int k, int *n_passos) {
+    passo * vet_passos = (passo *)calloc(n*m, sizeof(passo));
+    if(vet_passos == NULL){
+        perror("Erro ao alocar vetor de passos");
+        exit(1);
+    }
+    posicao ** matriz1 = (posicao**)calloc(n, sizeof(posicao*));
+    // Matriz aux
+    if(matriz1 == NULL){
+        perror("Erro ao alocar vetor de int");
+        exit(1);
+    }
+    for(int i = 0; i < n; i++){
+        matriz1[i] = (posicao*)calloc(m, sizeof(posicao));
+        if(matriz1[i] == NULL){
             perror("Erro ao alocar vetor de int");
             exit(1);
         }
-        for(int i = 0; i < n; i++){
-            matriz1[i] = (posicao*)calloc(m, sizeof(posicao));
-            if(matriz1[i] == NULL){
-                perror("Erro ao alocar vetor de int");
-                exit(1);
-            }
-        }
-
+    }
+    int preenchidos = 0;
+    int passos = 0;
+    while(preenchidos < m*n){
         for(int i = 0; i < n; i++)
             for(int l = 0; l < m; l++)
                 matriz1[i][l].cor = mat[i][l].cor;
@@ -209,23 +232,31 @@ void search_corners(posicao ** mat, int n, int m, int k) {
                     mat[i][l].teste = chosen_val;
                 }
 
+        
         if(chosen_i == 0 && chosen_j == 0)
-            printf("a ");
+            vet_passos[passos].canto = 'a';
         else if(chosen_i == 0 && chosen_j == m-1)
-            printf("b ");
+            vet_passos[passos].canto = 'b';
         else if(chosen_i == n-1 &&  chosen_j == 0)
-            printf("c ");
+            vet_passos[passos].canto = 'd';
         else if(chosen_i == n-1 &&  chosen_j == m-1)
-            printf("d ");
-        printf("%d\n", chosen_val);
-        usleep(500*100);
+            vet_passos[passos].canto = 'c';
+        vet_passos[passos].cor = chosen_val;
+        passos++;
     }
+
+    for (int i = 0; i < n; i++)
+        free(matriz1[i]);
+    free(matriz1);
+    *n_passos = passos;
+    return vet_passos;
 }
 
 int main(int argc, char **argv){
     int n = 0;
     int m = 0;
     int k = 0;
+    int n_passos = 0;
     posicao ** arquivo;
     FILE * nome_arquivo;
 
@@ -236,11 +267,17 @@ int main(int argc, char **argv){
 
     arquivo = lerArquivo(&n, &m, &k, nome_arquivo);
 
-    search_corners(arquivo, n, m, k);
+    passo * vet_passos = search_corners(arquivo, n, m, k, &n_passos);
+
+    printf("%d\n", n_passos);
+    for(int i = 0; i < n_passos; i++)
+        printf("%c %d ", vet_passos[i].canto, vet_passos[i].cor);
+    printf("\n");
 
     for (int i = 0; i < n; i++)
         free(arquivo[i]);
     free(arquivo);
+    free(vet_passos);
 
     return 0;
 }
